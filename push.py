5f9fe4403a1ff9cdb1e1e1bec5f81b67f266e325
fengyunxiren <fengyunxiren@163.com>
2016-04-26 20:21:12 +0800

    second push

--- push.py
+++ push.py
@@ -6,6 +6,9 @@ from django.core.management import setup_environ
 from djobs import settings
 from simpleblog import models
 setup_environ(settings)
+def addAuthor(getname,getemail='',getwebsite=''):
+    p=models.Author(name=getname,email=getemail,website=getwebsite)
+    p.save()
 
-p1=models.Author(name='cheetah',email='cheetah@163.com')
-p1.save()
+addAuthor('rain','rain@163.com')
+
